34c0927885f2e72c9a2b0b5d867ba8cec796bc67
Mwai <machariamwai@gmail.com>
2022-04-08 07:50:05 +0300

    commit tree 3dbb18137ef74d1ea7b6947a478d95a56974b14d

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+// distance from HQ in blocks
+let hqBlock = 42;
+function distanceFromHqInBlocks(blockNumber) {
+  let distance;
+
+  if (blockNumber >= 42) {
+    distance = blockNumber - hqBlock;
+  } else {
+    distance = Math.abs(blockNumber - hqBlock);
+  }
+  return distance;
+}
+
+const distance = distanceFromHqInBlocks(32);
+console.log(distance + " blocks");
+
+//distance from HQ in feet
+function distanceFromHqInFeet(blockNumber) {
+  return (Math.abs(distanceFromHqInBlocks(blockNumber)) * 264);
+}
+
+console.log(distanceFromHqInFeet(50) + " feet")
+
+//distance travelled in Feet
+function distanceTravelledInFeet(start, end){
+return(Math.abs(end-start)*264);
+}
+
+
+function calculatesFarePrice(start, end){
+ 
+  if(distanceTravelledInFeet(start, end) <= 400){
+    return 0;
+  }
+  else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) <= 2000){
+    return (0.02 *(distanceTravelledInFeet(start, end) -400))
+  }
+  else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) <= 2500){
+    return 25;
+  }
+  else if (distanceTravelledInFeet(start, end) > 2500){
+    return 'cannot travel that far';
+  }
+
+}
+
